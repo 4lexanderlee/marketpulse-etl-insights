99c57cbb16280bb94637a07c33ab1b20507613f5
Alexander Lee <melgarejorom@gmail.com>
2026-02-10 19:06:19 -0500

    ET2 - Mejore el script de extacción, agrege robustes y mejoras claves, log. de reintentos, validacion, manejo de rutas, tiempo de espera al servidor

--- src/extract.py
+++ src/extract.py
@@ -1,49 +1,76 @@
-import pandas_datareader.data as web
+import pandas_datareader.data as web 
 import os
 import time
 from datetime import datetime
 from pathlib import Path
 
-def download_market_data_stooq(tickers):
-    
-    # Detectamos la raíz del proyecto (un nivel arriba de 'src')
+def get_output_path():
+    """Configura y retorna la ruta de la carpeta raw"""
     base_path = Path(__file__).resolve().parent.parent
     output_path = base_path / "data" / "raw"
+    output_path.mkdir(parents=True, exist_ok=True)
+    return output_path
+
+def validate_data(df):
+    """Verifica si el DF contiene la información necesaria"""
+    if df is None or df.empty:
+        return False
+    #Esta columnas deben estar si o si en el dataframe
+    required_columns = ['Open', 'High', 'Low', 'Close']
     
-    if not output_path.exists():
-        output_path.mkdir(parents=True)
-    
-    # Definimos el rango de fechas
+    #El siguiente bloque se puede reducir a esto
+    #return all(col in df.columns for col in required_columns)
+    for col in required_columns:
+        if col not in df.columns:
+            return False
+        
+    return True
+
+def download_market_data_stooq(tickers, max_retries=3):
+    """Extrae datos de stooq, manejo de lógica de reintentos y validación"""
+    output_path = get_output_path()
     start = datetime(2021, 1, 1)
     end = datetime.now()
     
-    print(f"--- Iniciando extracción vía STOOQ  ---")
+    print(f"======== INICIANDO EXTRACCIÓN DE DATOS VÍA STOOQ ========")
     
     for ticker in tickers:
-        try:
-            print(f"Solicitando {ticker} de Stooq...")
-            
-            # Stooq usa nombres ligeramente distintos para índices
-            # Para el S&P 500 es '^SPX'
-            search_ticker = '^SPX' if ticker == 'VOO' or ticker == '^GSPC' else ticker
-            
-            df = web.DataReader(search_ticker, 'stooq', start, end)
-            
-            if not df.empty:
-                # Stooq devuelve los datos del más reciente al más antiguo, los invertimos
-                df = df.sort_index()
+        success = False
+        retries = 0
+        
+        #Mapeando al S&P 500 usando stooq
+        search_ticker = '^SPX' if ticker in ['VOO', '^GSPC'] else ticker 
+        
+        while not success and retries < max_retries:
+            try: 
+                print(f"   Solicitando {ticker} (como {search_ticker}). Intento {retries + 1}... ")
                 
-                file_name = f"{output_path}/{ticker}_historical.csv"
-                df.to_csv(file_name)
-                print(f"✅ ¡Éxito! {ticker} guardado en {file_name}")
+                #Descarga de datos
+                df = web.DataReader(search_ticker, 'stooq', start, end)
+                
+                if validate_data(df):
+                    #Invertimos los datos de manera ascendente
+                    df = df.sort_index()
+                    
+                    file_path = output_path / f"{ticker}_historico.csv"
+                    df.to_csv(file_path)
+                    
+                    print(f"✅ ¡Éxito! {ticker} guardado en: {file_path}")
+                    success = True
+                else:
+                    print(f"⚠️ Datos inválidos recibidos para {ticker}.")
+                    retries += 1
+            except Exception as e:
+                retries += 1
+                print(f"❌ Error al extraer {ticker}: {e}")
+                if retries < max_retries:
+                    print(f"Reintentando en 5 segundos...")
+                    time.sleep(5)
             
-            # Pausa de cortesía
+            #Pausa entre ticker para simular comportamiento humano y que no bloqueen al IP
             time.sleep(3)
-            
-        except Exception as e:
-            print(f"❌ Error con {ticker}: {e}")
-
-if __name__ == "__main__":
-    # Usaremos los nombres de tus ETFs elegidos
+    print(f"======== EXTRACIÓN FINALIZADA ========")
+    
+if __name__ == '__main__':
     assets = ["VOO", "QQQ", "SCHD"]
     download_market_data_stooq(assets)
